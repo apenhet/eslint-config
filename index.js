922d3fb5152b7dafa1203929ab6b64e5357f87a5
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2023-06-01 13:59:28 +0200

    chore: update and simplify

--- index.js
+++ index.js
@@ -1,137 +1,69 @@
-require('@rushstack/eslint-patch/modern-module-resolution')
-
-let isVue2 = false
-
-try {
-    isVue2 = require('vue').version.startsWith('2')
-} catch {
-    // Nothing
-}
-
-const _extends = [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'prettier'
-]
-
-const _plugins = ['sort-imports-es6-autofix', 'unused-imports']
-
-const _prettier = {
-    singleQuote: true,
-    trailingComma: 'none',
-    printWidth: 80,
-    tabWidth: 4,
-    semi: false,
-    arrowParens: 'avoid',
-    plugins: ['prettier-plugin-tailwindcss']
-}
-
 module.exports = {
     root: true,
-    env: {
-        node: true,
-        browser: true
-    },
-    extends: _extends,
     parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module'
+        extraFileExtensions: ['.vue']
     },
-    plugins: _plugins,
-    rules: {
-        'sort-imports-es6-autofix/sort-imports-es6': ['error'],
-        'unused-imports/no-unused-imports': ['error'],
-        'unused-imports/no-unused-vars': [
-            'error',
-            {
-                vars: 'all',
-                varsIgnorePattern: '^_',
-                args: 'after-used',
-                argsIgnorePattern: '^_'
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:vue/vue3-recommended',
+        'plugin:prettier/recommended',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'autofix'],
+    overrides: [
+        {
+            files: ['**/*.vue'],
+            parser: 'vue-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser'
             }
-        ],
-        'no-unused-vars': [
+        }
+    ],
+    rules: {
+        'no-undef': 'off',
+        'prefer-const': 'error',
+        'sort-imports': 'error',
+        'autofix/no-unused-vars': [
             'error',
             {
                 argsIgnorePattern: '^_',
                 varsIgnorePattern: '^_'
             }
         ],
-        'prettier/prettier': ['error', _prettier],
-        'prefer-template': ['error']
-    },
-    overrides: [
-        {
-            files: ['**/*.spec.js'],
-            rules: {
-                'no-undef': 'off'
+        'autofix/sort-vars': 'error',
+        'prefer-template': 'error',
+        'prettier/prettier': [
+            'error',
+            {
+                trailingComma: 'none',
+                tabWidth: 4,
+                semi: false,
+                singleQuote: true,
+                printWidth: 80,
+                arrowParens: 'avoid',
+                singleAttributePerLine: true
             }
-        },
-        {
-            files: ['**/*.vue'],
-            parser: 'vue-eslint-parser',
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-                ecmaVersion: 'latest',
-                sourceType: 'module'
-            },
-            extends: [
-                'eslint:recommended',
-                isVue2
-                    ? 'plugin:vue/recommended'
-                    : 'plugin:vue/vue3-recommended',
-                'plugin:prettier-vue/recommended',
-                'prettier'
-            ],
-            rules: {
-                'no-undef': 'off',
-                'prettier/prettier': ['off'],
-                'prettier-vue/prettier': ['error', _prettier],
-                'vue/no-v-html': 'off',
-                'vue/padding-line-between-blocks': ['error', 'always'],
-                'vue/block-tag-newline': ['error'],
-                'vue/custom-event-name-casing': ['error', 'kebab-case'],
-                'vue/html-button-has-type': ['error'],
-                'vue/next-tick-style': ['error', 'promise'],
-                'vue/no-duplicate-attr-inheritance': ['error'],
-                'vue/no-export-in-script-setup': ['error'],
-                'vue/no-unused-properties': ['error'],
-                'vue/no-unused-refs': ['error'],
-                'vue/multi-word-component-names': 'off',
-                'vue/require-default-prop': 'off'
+        ],
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/array-type': [
+            'error',
+            {
+                default: 'array-simple'
             }
-        },
-        {
-            files: ['**/*.ts', '**/*.tsx'],
-            parser: '@typescript-eslint/parser',
-            parserOptions: { project: ['./tsconfig.json'] },
-            extends: [
-                ..._extends,
-                'plugin:@typescript-eslint/recommended',
-                'plugin:@typescript-eslint/recommended-requiring-type-checking'
-            ],
-            plugins: [
-                'sort-imports-es6-autofix',
-                'unused-imports',
-                '@typescript-eslint'
-            ],
-            rules: {
-                'no-undef': 'off',
-                '@typescript-eslint/explicit-module-boundary-types': 'off',
-                '@typescript-eslint/no-unsafe-assignment': 'off',
-                '@typescript-eslint/no-unsafe-call': 'off',
-                '@typescript-eslint/no-unsafe-return': 'off',
-                '@typescript-eslint/no-unsafe-member-access': 'off',
-                '@typescript-eslint/no-explicit-any': 'off',
-                '@typescript-eslint/unbound-method': 'off',
-                '@typescript-eslint/no-unused-vars': [
-                    'error',
-                    {
-                        argsIgnorePattern: '^_',
-                        varsIgnorePattern: '^_'
-                    }
-                ]
-            }
-        }
-    ]
+        ],
+        '@typescript-eslint/method-signature-style': ['error', 'method'],
+        '@typescript-eslint/naming-convention': 'error',
+        '@typescript-eslint/consistent-indexed-object-style': [
+            'error',
+            'record'
+        ],
+        '@typescript-eslint/prefer-includes': 'error',
+        '@typescript-eslint/prefer-ts-expect-error': 'error',
+        'vue/no-setup-props-destructure': 'off',
+        'vue/no-v-html': 'off',
+        'vue/multi-word-component-names': 'off'
+    }
 }
