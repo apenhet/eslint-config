bd9fd6811e16b38969614b41e5c15f01a5e35f52
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2024-06-03 11:26:26 +0200

    feat: bump to flat config

--- index.mjs
+++ index.mjs
@@ -2,7 +2,8 @@ import unusedImports from 'eslint-plugin-unused-imports'
 import stylistic from '@stylistic/eslint-plugin'
 import tailwindcss from 'eslint-plugin-tailwindcss'
 import nuxt from '@nuxt/eslint-plugin'
-import tseslint from 'typescript-eslint'
+import parserTs from '@typescript-eslint/parser'
+import pluginTs from '@typescript-eslint/eslint-plugin'
 import vue from 'eslint-plugin-vue'
 
 export default [
@@ -13,53 +14,41 @@ export default [
         arrowParens: true,
         blockSpacing: true,
         braceStyle: '1tbs',
-        commaDangle: 'never'
-    }), {
-        plugins: {
-            '@typescript-eslint': tseslint.plugin,
-            tailwindcss: tailwindcss,
-            'unused-imports': unusedImports,
-            'nuxt': nuxt
+        commaDangle: 'never',
+    }),
+    ...vue.configs['flat/recommended'],
+    {
+        files: [
+            '**/*.ts'
+        ],
+        languageOptions: {
+            parser: parserTs,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module'
+            }
         },
+    },
+    {
+        files: ['**/*.vue'],
         languageOptions: {
             parserOptions: {
-                parser: tseslint.parser,
+                parser: parserTs,
+                ecmaVersion: 'latest',
                 extraFileExtensions: ['.vue'],
                 sourceType: 'module'
             }
-        }
-    },
-    ...vue.configs['flat/recommended'],
-    {
+        },
         rules: {
             'vue/multi-word-component-names': 'off',
             'vue/no-multiple-template-root': 'off',
-            'unused-imports/no-unused-imports': 'error',
-            'unused-imports/no-unused-vars': [
-                'warn',
-                {
-                    vars: 'all',
-                    varsIgnorePattern: '^_',
-                    args: 'after-used',
-                    argsIgnorePattern: '^_'
-                }
-            ],
-            'no-undef': 'off',
-            'prefer-const': 'error',
-            'sort-vars': 'error',
-            'prefer-template': 'error',
-            'nuxt/prefer-import-meta': 'error',
-            'no-multiple-empty-lines': [
-                'error',
-                {
-                    max: 1,
-                    maxBOF: 0,
-                    maxEOF: 0
-                }
-            ],
-            'tailwindcss/classnames-order': 'error',
-            'tailwindcss/no-custom-classname': 'error',
-            'tailwindcss/no-contradicting-classname': 'error',
+        }
+    },
+    {
+        plugins: {
+            '@typescript-eslint': pluginTs,
+        },
+        rules: {
             '@typescript-eslint/consistent-indexed-object-style': [
                 'error',
                 'record'
@@ -70,6 +59,67 @@ export default [
             '@typescript-eslint/no-unsafe-call': 'off',
             '@typescript-eslint/no-unsafe-return': 'off',
             '@typescript-eslint/prefer-ts-expect-error': 'error'
+        }
+    },
+    {
+        name: 'imports',
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+        rules: {
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_'
+                }
+            ],
+        }
+    },
+    {
+        name: 'tailwindcss',
+        plugins: {
+            tailwindcss: tailwindcss,
+        },
+        rules: {
+
+            'tailwindcss/classnames-order': 'error',
+            'tailwindcss/no-custom-classname': 'error',
+            'tailwindcss/no-contradicting-classname': 'error',
+        }
+    },
+     {
+        name: 'nuxt',
+        plugins: {
+            'nuxt': nuxt
+        },
+        rules: {
+            'nuxt/prefer-import-meta': 'error',
+        }
+    },
+    {
+        rules: {
+            'no-undef': 'off',
+            'prefer-const': 'error',
+            'sort-vars': 'error',
+            'prefer-template': 'error',
+            'no-multiple-empty-lines': [
+                'error',
+                {
+                    max: 1,
+                    maxBOF: 0,
+                    maxEOF: 0
+                }
+            ],
+            '@stylistic/array-element-newline': [
+                'error',
+                'always'
+            ],
+            '@stylistic/array-bracket-newline': ["error", {  "minItems": 2  }],
+            '@stylistic/array-bracket-spacing': ["error", "never"]
         }
     }
 ]
